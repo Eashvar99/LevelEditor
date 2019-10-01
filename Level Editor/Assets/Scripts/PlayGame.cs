983895b6eb7a6aad07ffe5e2846e8280790e9369
Eashvar Balachandran <eashvar.balachandran@uoit.net>
2019-10-01 16:02:17 -0400

    Play Game updates Camera

---
+++ Level Editor/Assets/Scripts/PlayGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayGame : MonoBehaviour
+{  
+    public Camera cam;
+
+    public void changeCamera()
+    {
+        cam.transform.localPosition = new Vector3(-0.8f, 15.4f, -27.8f);
+
+    cam.transform.eulerAngles = new Vector3(
+    cam.transform.eulerAngles.x - 63.94f,
+    cam.transform.eulerAngles.y,
+    cam.transform.eulerAngles.z);
+
+    }
+}
